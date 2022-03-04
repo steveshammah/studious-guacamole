665ab06e1a922fe60a18b459e3321ac861b59f61
steveshammah <shammahodhiambo@gmail.com>
2022-03-04 22:17:20 +0300

    refactor: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <Box >
+      <Head>
+        <title>The Good Network</title>
+        <meta name="description" content="The Good Network" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+    </Box>
+  )
+}
+
+export default Home
