e602802e063275ad3896f329b72b10ec0444ebd7
steveshammah <shammahodhiambo@gmail.com>
2022-03-06 16:07:45 +0300

    feat: cookies toaster

---
+++ components/CookesPolicy.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const CookesPolicy = () => {
+  const [active, setActive] = useState(true);
+
+  return (
+    <Box
+      position={"absolute"}
+      bottom={0}
+      bg={"black"}
+      w={"100%"}
+      p={3}
+      justifyContent={"space-around"}
+      alignItems={"center"}
+      display={active ? "flex" : "none"}
+    >
+      <Text color={"white"} maxW={"80%"}>
+        We use cookies on our website to give you the most relevant experience
+        by remembering your preferences and repeat visits. By clicking “Accept”,
+        you consent to the use of ALL the cookies.
+      </Text>
+      <Button>Accept</Button>
+      <Text onClick={() => setActive(!active)}>Close</Text>
+    </Box>
+  );
+};
+
+export default CookesPolicy;
